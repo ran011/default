31e3b22047a771f9466d26eedaac61704beb31c3
ran011 <59457935+ran011@users.noreply.github.com>
2020-02-22 11:31:58 -0800

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# default
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div class="bigbox">
+        <h1>Ambedu</h1>
+      </div>
+    </div>
+  );
+}
